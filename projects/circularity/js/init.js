f8eaf9bfbdc526f6263d38c8ea68ccdc057c8e60
jackso636 <157505985+jackso636@users.noreply.github.com>
2024-05-07 14:02:01 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -1,13 +1,5 @@
-import { init } from "./init";
 
-// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if (
-  typeof process !== "undefined" &&
-  typeof process.versions.node !== "undefined"
-) {
-  // here, export any references you need for tests //
-  module.exports = init;
-};var init = function (window) {
+var init = function (window) {
     'use strict';
     var
         draw = window.opspark.draw, physikz = window.opspark.racket.physikz, app = window.opspark.makeApp(), canvas = app.canvas, view = app.view, fps = draw.fps('#000');
@@ -26,19 +18,29 @@ if (
         var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-       
+       function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-            physikz.addRandomVelocity(circle, canvas, 10, 10);
+            physikz.addRandomVelocity(circle, canvas);
             view.addChild(circle);
             circles.push(circle);
+       }
         
         // TODO 3 / 7 : Call the drawCircle() function 
+       var loopsCompleted = 10;
+       while (loopsCompleted < 100){
+        drawCircle();
+        loopsCompleted++;
+       }
+    
+    for (var loopsCompleted = 10; loopsCompleted < 10 ; loopsCompleted++){
+        drawCircle();
+    }
         drawCircle();
         drawCircle();
         drawCircle();
         drawCircle();
         drawCircle();
-        for (drawCircle = i; i < 100; i++)
+       
             ////////////////////////////////////////////////////////////
             ///////////////// PROGRAM LOGIC ////////////////////////////
             ////////////////////////////////////////////////////////////
@@ -48,21 +50,18 @@ if (
             and check to see if it has drifted off the screen.
             */
             function update() {
+                for (var i = 0; i < circles.length; i++) {
+                    physikz.updatePosition(circles[i]);
+                    game.checkCirclePosition(circles[i]);
+                }
+            }
                 // TODO 4 : Update the circle's position //
-                physikz.updatePosition(circles[0]);
-                physikz.updatePosition(circles[1]);
-                physikz.updatePosition(circles[2]);
-                physikz.updatePosition(circles[3]);
-                physikz.updatePosition(circles[4]);
+             
 
 
 
                 // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-                game.checkCirclePosition(circles[0]);
-                game.checkCirclePosition(circles[1]);
-                game.checkCirclePosition(circles[2]);
-                game.checkCirclePosition(circles[3]);
-                game.checkCirclePosition(circles[4]);
+             
 
                 // TODO 9 : Iterate over the array
             }
@@ -72,18 +71,25 @@ if (
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
-        game.checkCirclePosition = function (circle) {
-            canvas.width;
-            canvas.height;
-
-            circle.x;
-            circle.y;
-            circle.radius;
+        game.checkCirclePosition = function(circle) {
+        
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
             if (circle.x > canvas.width) {
                 circle.x = 0;
             }
+            if (circle.x < 0){
+                circle.x = canvas.with;
+            }
+
+            if ( circle.y < 0){
+                circle.y = canvas.height;
+            }
+
+            
+             if ( circle.y > canvas. height){
+                circle.y = 0;
+             }
 
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
@@ -102,5 +108,9 @@ if (
 
         app.addUpdateable(window.opspark.game);
     };
-};
+    
 
+((typeof process !== 'undefined'))&&
+((typeof process.versions.node !== undefined)) {
+    module.exports = init;
+}
